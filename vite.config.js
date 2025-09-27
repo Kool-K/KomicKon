9cbdbab6fb10ba4748c7270d5a4334f523f0ec05
Ketaki <ketakisk2018@gmail.com>
2025-09-27 13:13:37 +0530

    workflow changes

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
    base: './',
+   // Add this for proper asset handling
+   build: {
+     outDir: 'dist',
+     assetsDir: 'assets'
+   }
 })
